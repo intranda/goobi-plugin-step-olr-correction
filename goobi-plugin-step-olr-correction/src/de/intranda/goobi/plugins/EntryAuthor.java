95f65692a43d56bcd28c307f3dbc15c075ec5845
Oliver Paetzel <oliver.paetzel@intranda.com>
2019-10-01 16:59:56 +0200

    fix strange PICA-generation bug

--- goobi-plugin-step-olr-correction/src/de/intranda/goobi/plugins/EntryAuthor.java
+++ goobi-plugin-step-olr-correction/src/de/intranda/goobi/plugins/EntryAuthor.java
@@ -3,19 +3,19 @@ package de.intranda.goobi.plugins;
 import lombok.Getter;
 
 public class EntryAuthor {
-	@Getter
-	private String fullName;
-	
-	public EntryAuthor(String fullName) {
-//		this.fullName = fullName;
-		this.fullName = fullName.replaceAll("(\\.)(\\w)", "$1 $2");
-	}
-	
-	public String getPicaName() {
-		return replaceReverse(fullName, " ", "@").trim();
-	}
-	
-	private String replaceLast(String string, String toReplace, String replacement) {
+    @Getter
+    private String fullName;
+
+    public EntryAuthor(String fullName) {
+        //		this.fullName = fullName;
+        this.fullName = fullName.replaceAll("(\\.)(\\w)", "$1 $2");
+    }
+
+    public String getPicaName() {
+        return replaceReverse(fullName, " ", "@").trim();
+    }
+
+    private String replaceLast(String string, String toReplace, String replacement) {
         int pos = string.lastIndexOf(toReplace);
         if (pos > -1) {
             return string.substring(0, pos) + replacement + string.substring(pos + toReplace.length(), string.length());
@@ -23,13 +23,14 @@ public class EntryAuthor {
             return string;
         }
     }
-	
-	private String replaceReverse(String string, String toReplace, String replacement) {
-        int pos = string.lastIndexOf(toReplace);
+
+    private String replaceReverse(String string, String toReplace, String replacement) {
+        String trimmed = string.trim();
+        int pos = trimmed.lastIndexOf(toReplace);
         if (pos > -1) {
-        		return string.substring(pos + toReplace.length(), string.length()) + ", " + string.substring(0, pos).trim();
+            return trimmed.substring(pos + toReplace.length(), trimmed.length()) + ", " + trimmed.substring(0, pos).trim();
         } else {
-            return string;
+            return trimmed;
         }
     }
 }
