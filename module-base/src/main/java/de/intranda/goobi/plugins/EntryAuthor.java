5b119070fab9a4ce20cb32e6623d65cabd11c02d
Simon Sendler <simon.sendler@intranda.com>
2026-01-21 09:50:38 +0100

    refactor: remove unused methods

--- module-base/src/main/java/de/intranda/goobi/plugins/EntryAuthor.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/EntryAuthor.java
@@ -78,23 +78,4 @@ public class EntryAuthor {
     private boolean isInitial(String str) {
         return str.matches("([A-Za-z]\\.)+");
     }
-
-    private String replaceLast(String string, String toReplace, String replacement) {
-        int pos = string.lastIndexOf(toReplace);
-        if (pos > -1) {
-            return string.substring(0, pos) + replacement + string.substring(pos + toReplace.length(), string.length());
-        } else {
-            return string;
-        }
-    }
-
-    private String replaceReverse(String string, String toReplace, String replacement) {
-        String trimmed = string.trim();
-        int pos = trimmed.lastIndexOf(toReplace);
-        if (pos > -1) {
-            return trimmed.substring(pos + toReplace.length(), trimmed.length()) + ", " + trimmed.substring(0, pos).trim();
-        } else {
-            return trimmed;
-        }
-    }
 }
